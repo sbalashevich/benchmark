cbca31bed59a29bff69cf4d61af0b4f553a8538a
Siarhei Balashevich <balashevich@gmail.com>
2018-10-14 22:06:23 +0300

    configtx.yaml

--- jumpboxinit.sh
+++ jumpboxinit.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+
+CHANNEL_NAME="mychannel"
+
+
+export LOCAL_FOLDER=/benchmark/crypto-config
+export FABRIC_CFG_PATH=/benchmark
+
+
 cryptogen generate --config=./crypto/crypto-config.yml
+mkdir channel-artifacts
+
+configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
+
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
 
 cp -r crypto-config/ordererOrganizations /orderer/
+cp -r ./channel-artifacts /orderer/
+
