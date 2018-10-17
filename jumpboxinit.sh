ddb9b60d884a60c7efe05c49e986e7d9837c5fd6
Siarhei Balashevich <balashevich@gmail.com>
2018-10-17 22:47:10 +0300

    configuration for peer 0 org 1

--- jumpboxinit.sh
+++ jumpboxinit.sh
@@ -17,6 +17,12 @@ configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/c
 configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
 configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
 
+# ORDERER
 cp -r crypto-config/ordererOrganizations /orderer/
 cp -r ./channel-artifacts /orderer/
 
+# Org1Peer0
+#        - ../crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/msp:/etc/hyperledger/fabric/msp
+#        - ../crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls:/etc/hyperledger/fabric/tls
+cp -r crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/* /org1peer0/
+cp -r ./channel-artifacts /org1peer0/
