193e8286f67b9461e3bf4e837e340ac02c672755
Siarhei Balashevich <balashevich@gmail.com>
2018-10-21 23:57:25 +0300

    copy crypto config using local drive

--- clean.sh
+++ clean.sh
@@ -7,12 +7,24 @@ kubectl delete deploy/fabric-cli
 
 kubectl delete pvc/orderer-claim
 kubectl delete pvc/org1peer0-claim
+kubectl delete pvc/orderer-claim --namespace=master
+kubectl delete pvc/org1peer0-claim --namespace=master
 
 kubectl delete pv/orderer
 kubectl delete pv/org1peer0
+kubectl delete pv/orderer  --namespace=org1
+kubectl delete pv/org1peer0  --namespace=org1
+
+kubectl delete svc/orderer --namespace=master
+kubectl delete svc/org1peer0 --namespace=org1
+
 
 kubectl delete namespace orderer
 kubectl delete namespace org1
 kubectl delete namespace org2
 
-
+rm -rf /tmp/orderer/*
+rm -rf /tmp/org1peer0/*
+rm -rf /tmp/org1peer1/*
+rm -rf /tmp/org2peer0/*
+rm -rf /tmp/org2peer1/*
--- start.sh
+++ start.sh
@@ -1,15 +1,33 @@
 #!/usr/bin/env bash
 
+mkdir -p /tmp/orderer
+mkdir -p /tmp/org1peer0
+mkdir -p /tmp/org1peer1
+mkdir -p /tmp/org2peer0
+mkdir -p /tmp/org2peer1
+
+
 kubectl apply -f deployment/namespaces.yml
 
-kubectl apply -f deployment/volumes/pvc_orderer.yml
-kubectl apply -f deployment/volumes/pvc_org1peer0.yml
+sleep 2
+
+kubectl apply -f deployment/volumes.yml
 
 sleep 5
 
 kubectl apply -f deployment/jump-box.yml
 
-sleep 10
+sleep 20
+
+cp -r /tmp/orderer/* /tmp/orderer_master/
+cp -r /tmp/org1peer0/* /tmp/org1peer0_master/
+
+kubectl apply -f deployment/orderer_withnamespaces.yml
+
+sleep 2
+
 
 kubectl apply -f deployment/orderer.yml
-kubectl apply -f deployment/org1peer0.yml
+
+
+#kubectl apply -f deployment/org1peer0.yml
